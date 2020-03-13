6b11411fe31c585906aadef269794ce17f83413b
Manuel Braun <mbraun@conet.de>
2020-03-13 17:43:04 +0100

    Refactoring

--- GraphApi/src/module.ts
+++ GraphApi/src/module.ts
@@ -1,6 +1,5 @@
 import "isomorphic-fetch";
-import { Client, ClientOptions } from "@microsoft/microsoft-graph-client";
-import { GraphAuthenticationProvider } from "./authProvider";
+import { getAuthenticatedGraphClient, checkSharepointParams } from "./utils";
 
 /**
  * Get Sharepoint list by ID / name
@@ -31,36 +30,6 @@ async function getSharePointList(
 }
 
 module.exports.getSharePointList = getSharePointList;
-
-function getAuthenticatedGraphClient(secret: IGraphSecret): Client {
-  const clientOptions: ClientOptions = {
-    authProvider: new GraphAuthenticationProvider(
-      secret.tenantId,
-      secret.clientId,
-      secret.clientSecret
-    )
-  };
-
-  return Client.initWithMiddleware(clientOptions);
-}
-
-function checkSharepointParams(args: ISharePointArgs): Promise<never> {
-  const { secret, siteCollectionHost, siteName, listName, contextStore } = args;
-  if (!secret || !secret.tenantId || !secret.clientId || !secret.clientSecret)
-    return Promise.reject("Secret not defined or invalid.");
-
-  if (!siteCollectionHost)
-    return Promise.reject("No siteCollectionsHost defined.");
-
-  if (!siteName) return Promise.reject("No siteName / siteID defined.");
-
-  if (!listName) return Promise.reject("No listName / listID defined.");
-
-  if (!contextStore)
-    return Promise.reject(
-      "No context store defined. This is needed to save results."
-    );
-}
 
 // Get all lists per site
 // client.api(`/sites/${siteCollectionHost}:/sites/${siteName}:/lists`).get()
---
+++ GraphApi/src/utils.ts
@@ -0,0 +1,32 @@
+import { Client, ClientOptions } from "@microsoft/microsoft-graph-client";
+import { GraphAuthenticationProvider } from "./authProvider";
+
+export function getAuthenticatedGraphClient(secret: IGraphSecret): Client {
+  const clientOptions: ClientOptions = {
+    authProvider: new GraphAuthenticationProvider(
+      secret.tenantId,
+      secret.clientId,
+      secret.clientSecret
+    )
+  };
+
+  return Client.initWithMiddleware(clientOptions);
+}
+
+export function checkSharepointParams(args: ISharePointArgs): Promise<never> {
+  const { secret, siteCollectionHost, siteName, listName, contextStore } = args;
+  if (!secret || !secret.tenantId || !secret.clientId || !secret.clientSecret)
+    return Promise.reject("Secret not defined or invalid.");
+
+  if (!siteCollectionHost)
+    return Promise.reject("No siteCollectionsHost defined.");
+
+  if (!siteName) return Promise.reject("No siteName / siteID defined.");
+
+  if (!listName) return Promise.reject("No listName / listID defined.");
+
+  if (!contextStore)
+    return Promise.reject(
+      "No context store defined. This is needed to save results."
+    );
+}
